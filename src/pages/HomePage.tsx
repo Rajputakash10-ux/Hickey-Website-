ab3e44e0dba1e4b38d559c71216016e9e5498061
Akash Rajput <Rajputakash10@icloud.com>
2026-08-09 15:54:35 +0530

    fix: remove moving photos (ImageMarquee) from homepage

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,6 +1,5 @@
 import type { Product } from '../types';
 import Hero from '../components/Hero';
-import ImageMarquee from '../components/ImageMarquee';
 import FeaturedProduct from '../components/FeaturedProduct';
 import ProductCollection from '../components/ProductCollection';
 import HowItWorks from '../components/HowItWorks';
@@ -21,7 +20,6 @@ export default function HomePage({ featured, products, loading, onAddToCart, onC
   return (
     <main>
       <Hero onAddToCart={() => { onAddToCart(featured, 1); onCartOpen(); }} />
-      <ImageMarquee />
       <FeaturedProduct product={featured} loading={loading} onAddToCart={onAddToCart} />
       <ProductCollection products={products} loading={loading} onAddToCart={onAddToCart} />
       <HowItWorks />
